c2629e5b767f7112e02f47be6decb48d21af62ae
abhynadar <vabhynadar@yahoo.com>
2014-03-23 20:54:37 +0530

    added comments

--- HealthyHappenings.Contract/IHealthyHappeningService.cs
+++ HealthyHappenings.Contract/IHealthyHappeningService.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace HealthyHappenings.Contract
 {
+	/// <summary>
+	/// This service contract defines the methods need to be implemented for acting as a data source for health events
+	/// </summary>
 	public interface IHealthyHappeningService
 	{
 		List<Event> GetByCity(string city);
