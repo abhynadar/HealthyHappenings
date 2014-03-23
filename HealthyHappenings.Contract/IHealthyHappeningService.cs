2e7a6025e7745a4afeb42d96e8a2bfce39dab763
abhynadar <vabhynadar@yahoo.com>
2014-03-23 21:22:08 +0530

    added comments to the GetByCity method

--- HealthyHappenings.Contract/IHealthyHappeningService.cs
+++ HealthyHappenings.Contract/IHealthyHappeningService.cs
@@ -7,6 +7,11 @@ namespace HealthyHappenings.Contract
 {
 	public interface IHealthyHappeningService
 	{
+		/// <summary>
+		/// This method is used to provide us with the data of health events for a given city.
+		/// </summary>
+		/// <param name="city">city for which health events are required</param>
+		/// <returns>List of health events</returns>
 		List<Event> GetByCity(string city);
 	}
 }
